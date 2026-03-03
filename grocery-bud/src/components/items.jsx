28f4af83a63a0ea3724a927d5fe6d4cc3b840576
dibyaraj <dahaldibyaraj46@gmail.com>
2026-03-03 13:27:20 +0545

    made update in app.css

--- grocery-bud/src/components/items.jsx
+++ grocery-bud/src/components/items.jsx
@@ -1,12 +1,18 @@
-import singleitem from "./singleitem";
+import SingleItem from "./singleitem";
 import "./items.css";
 
-const Items = ({ items }) => {
+const Items = ({ items, editCompleted, deleteItem, startEdit }) => {
   return (
     <div className="items">
-      {items.map((item) => {
-        return <singleitem key={item.id} item={item} />;
-      })}
+      {items.map((item) => (
+        <SingleItem
+          key={item.id}
+          item={item}
+          editCompleted={editCompleted}
+          deleteItem={deleteItem}
+          startEdit={startEdit}
+        />
+      ))}
     </div>
   );
 };
--- grocery-bud/src/components/singleitem.jsx
+++ grocery-bud/src/components/singleitem.jsx
@@ -1,29 +1,23 @@
 import { FiEdit, FiTrash2 } from "react-icons/fi";
-import "./SingleItem.css";
+import "./singleitem.css";
 
-const SingleItem = ({ item }) => {
+const SingleItem = ({ item, editCompleted, deleteItem, startEdit }) => {
   return (
     <div className="single-item">
-      <input type="checkbox" checked={item.completed} onChange={() => null} />
-      <p
-        style={{
-          textTransform: "capitalize",
-          textDecoration: item.completed ? "line-through" : "none",
-        }}
-      >
-        {item.name}
-      </p>
+      <input
+        type="checkbox"
+        checked={item.completed}
+        onChange={() => editCompleted(item.id)}
+      />
 
-      <button className="btn icon-btn" type="button" onClick={() => null}>
-        <FiEdit size={18} />
+      <p className={item.completed ? "completed" : ""}>{item.name}</p>
+
+      <button onClick={() => startEdit(item)}>
+        <FiEdit />
       </button>
 
-      <button
-        className="btn icon-btn remove-btn"
-        type="button"
-        onClick={() => null}
-      >
-        <FiTrash2 size={18} />
+      <button onClick={() => deleteItem(item.id)}>
+        <FiTrash2 />
       </button>
     </div>
   );
